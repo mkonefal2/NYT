4363b8aa4d09f2fbff836470ab43a66836f8dcc0
Mkonefal <michalkonefal@hotmail.com>
2024-06-11 12:53:56 +0200

    commit update scripts

--- scripts/analyze_headlines.py
+++ scripts/analyze_headlines.py
@@ -1,37 +1,35 @@
-# Dodaj import modułu os
 import os
 import duckdb
 import pandas as pd
 import re
 from collections import Counter
 import datetime
-
-# Pełna ścieżka do pliku bazy danych
+# Full path to the database file
 db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/nyt_articles.db'))
 
 con = duckdb.connect(database=db_path)
 
-# Analiza najczęściej występujących słów w nagłówkach
+# Analyzing the most frequently occurring words in the headlines
 headlines = con.execute('SELECT headline FROM headline_analysis').fetchall()
 headlines = [str(headline[0]) for headline in headlines if headline[0] is not None]
 
-# Uzyskanie bieżącej daty i sformatowanie jej do postaci YYYY-MM-DD
+# Getting the current date and formatting it as YYYY-MM-DD
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-# Tokenizacja i czyszczenie tekstu nagłówków
+# Tokenizing and cleaning the headline text
 all_words = ' '.join(headlines)
 words = re.findall(r'\b\w+\b', all_words.lower())
 
-# Definiuj listę słów mało znaczących
+# Define a list of stopwords
 stopwords = {'the', 'a', 's', 'to', 'in','its','be' ,'has', 'it', 'and', 'of', 'for', 'on', 'with','t' , 'after','this', 'what', 'as', 'is', 'that', 'at', 'by', 'an', 'u', 'c', 'n', 'k' ,'i' , 'how' , 'no' , 'are' , 'from', 'you'}
-# Filtruj słowa, usuwając stopwords
+# Filter out stopwords from the words
 filtered_words = [word for word in words if word not in stopwords]
 
 word_count = Counter(filtered_words)
 common_words = pd.DataFrame(word_count.most_common(200), columns=['word', 'count'])
-# Zapisz DataFrame do pliku CSV
+# Save the DataFrame to a CSV file
 common_words.to_csv(os.path.join(os.path.dirname(__file__), f"../data/common_words_{current_date}.csv"), index=False)
 
 
-# Zapisanie przekształconych danych do pliku CSV
+# Save the transformed data to a CSV file
 print(f"Data saved to {os.path.join(os.path.dirname(__file__), f'../data/common_words_{current_date}.csv')}")
--- scripts/bar_chart.py
+++ scripts/bar_chart.py
@@ -1,18 +1,19 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import datetime
 
-# Załaduj DataFrame z pliku CSV
+import matplotlib.pyplot as plt
+
+# Load DataFrame from CSV file
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 csv_path = os.path.join(os.path.dirname(__file__), f"../data/common_words_{current_date}.csv")
 common_words_df = pd.read_csv(csv_path)
-# Filtracja do 10 najczęściej występujących słów
+# Filter for the top 10 most common words
 common_words_df = common_words_df.nlargest(10, 'count')
-# Generowanie wykresu z ciemniejszym tłem
+# Generate a bar chart with a darker background
 fig, ax = plt.subplots(figsize=(10, 8))
-fig.patch.set_facecolor('#232136')  # Ustawienie koloru tła dla całej figury
-ax.set_facecolor('#232136')  # Ustawienie koloru tła dla obszaru wykresu
+fig.patch.set_facecolor('#232136')  # Set the background color for the entire figure
+ax.set_facecolor('#232136')  # Set the background color for the chart area
 
 plt.bar(common_words_df['word'], common_words_df['count'], color='#ec9b99')
 plt.xlabel('Words', color='white')
@@ -22,13 +23,13 @@ plt.yticks(color='white')
 plt.title('Most Common Words in NYT Headlines', color='white')
 plt.tight_layout()
 
-# Zapisz wykres do pliku PNG w katalogu `plots`
+# Save the chart to a PNG file in the `plots` directory
 plots_dir = os.path.join(os.path.dirname(__file__), f"../plots")
 if not os.path.exists(plots_dir):
     os.makedirs(plots_dir)
 plot_path = os.path.join(plots_dir, f"common_words_plot_{current_date}.png")
 
-# Zapisz wykres do pliku PNG z ciemnym tłem
+# Save the chart to a PNG file with a dark background
 plt.savefig(plot_path, facecolor=fig.get_facecolor())
 
 plt.savefig(plot_path)
